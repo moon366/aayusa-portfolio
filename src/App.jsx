e7fd2faa16ba2df48d6097c14a0998790aa14b8a
moon366 <aayusaneupane49@gmail.com>
2026-08-08 00:45:59 +0545

    Redirect /ankitamiss route to sunflower gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Home from "./pages/Home";
-import Button from "./components/Button";
 import ScrollToTop from "./components/ScrollToTop";
 import MusicPlayer from "./components/MusicPlayer";
+
+const SUNFLOWER_URL = "https://aayusasunflower.vercel.app/#/gallery";
 
 function App() {
   const [showAnkitamiss, setShowAnkitamiss] = useState(false);
 
   useEffect(() => {
     const checkHash = () => {
-      setShowAnkitamiss(window.location.hash === "#ankitamiss");
+      const hash = window.location.hash;
+      if (hash === "#ankitamiss" || hash === "#/ankitamiss") {
+        window.location.replace(SUNFLOWER_URL);
+        setShowAnkitamiss(true);
+      }
     };
 
     checkHash();
@@ -18,7 +23,7 @@ function App() {
   }, []);
 
   if (showAnkitamiss) {
-    return <Button />;
+    return null;
   }
 
   return (
